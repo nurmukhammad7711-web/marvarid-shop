f4f872eb8f2b3078123f687c2771eba05f8346df
nurmukhammad7711-web <nurmukhammad7711@gmail.com>
2026-07-14 10:49:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("index.html", "r", encoding="utf-8") as f:
+        return f.read()
+
+# Это добавит поддержку порта от Render
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
